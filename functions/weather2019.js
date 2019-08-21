56c49a1911b8710db9cbb4a768b18ed4b9687fd0
doebi <christoph@doeberl.at>
2019-08-21 14:12:28 +0200

    wohoo

--- functions/weather2019.js
+++ functions/weather2019.js
@@ -3,13 +3,12 @@ const axios = require('axios');
 const key = process.env.DARKSKY;
 const url = `https://api.darksky.net/forecast/${key}/52.3451,5.4581?units=ca`;
 
+exports.handler = async function(event, context, callback) {
 
-exports.handler = function(event, context, callback) {
-
-  let data = axios.get(url).then((r) => r.data);
+  let data = await axios.get(url).then((r) => r.data);
 
   callback(null, {
     statusCode: 200,
-    body: JSON.stringify(data);
+    body: JSON.stringify(data)
   });
 }
